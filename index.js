2f5ed9fb7f498c0da5798a844c2d00ade7bf7b79
Simon Hammer <77269703+shft5410@users.noreply.github.com>
2024-05-19 20:05:23 +0200

    Add page header and improve styles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fileInputElement = document.querySelector('#file-input')
-const chartContainerElement = document.querySelector('.chart-container')
+const fileInputElement = document.querySelector('#header__file-input')
+const chartContainerElement = document.querySelector('.main__chart-container')
 
 /* Full palette: https://coolors.co/palette/0b090a-161a1d-660708-a4161a-ba181b-e5383b-b1a7a6-d3d3d3-f5f3f4-ffffff */
 const COLORS = {
